1e48cb1a14e502f1d5aa55ac173fdf3c4cbee510
nishi4321 <n.nishikita2@gmail.com>
2020-10-24 15:49:29 +0900

    Init commit.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# HTTPobserver
+# HTTP observer
+
+対象のURLに対して定期的にHTTPリクエストを投げ、その応答を監視するソフトウェア。  
+簡易的なWebサービスの死活監視が目的。
+
+# configuration
+`config.json` をルートディレクトリに作成してください。  
+設定項目については、同梱されている `config_sample.json` を参照してください。
+
+# preparation
+install required packages.
+```
+$ npm i
+```
+
+run app.
+```
+$ npm start
+```
+
+# LICENSE
+MIT License
---
+++ main.js
@@ -0,0 +1,126 @@
+const request = require('request');
+const schedule = require('node-schedule');
+var fs = require('fs');
+require('date-utils');
+
+// 
+var statusMap = new Map();
+
+// Read config.json
+const msg = fs.readFileSync("config.json", { encoding: "utf-8" });
+var config = JSON.parse(msg);
+
+// Init state map.
+var targetUrlList = config.target_url;
+for (const targetUrl of targetUrlList) {
+    statusMap.set(targetUrl, 200);
+}
+
+// 
+schedule.scheduleJob('0 * * * * *', () => {
+    checkHTTPstatus();
+});
+
+// 
+function checkHTTPstatus() {
+    var dt = new Date();
+    console.log("====" + dt.toFormat("YYYY/MM/DD HH24:MI:SS") + "====");
+    var targetUrlList = config.target_url;
+    for (const targetUrl of targetUrlList) {
+        getHTTPstatuscode(targetUrl).then(function (result) {
+            var isNotify = isChanged(targetUrl, result.code);
+            if (isNotify) {
+                sendNotification(targetUrl, result)
+            }
+            setStatusMap(targetUrl, result.code);
+            console.log(targetUrl, result, isNotify);
+        })
+    }
+}
+
+// 
+function isChanged(url, statusCode) {
+    var previousCode = statusMap.get(url);
+    if (previousCode != statusCode) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// 
+function setStatusMap(url, statusCode) {
+    statusMap.set(url, statusCode);
+}
+
+// 
+function sendNotification(url, result) {
+    var dt = new Date();
+    var formatted = dt.toFormat("YYYY/MM/DD HH24:MI:SS");
+    if (result.code == 200) {
+        var options = {
+            uri: config.slack_webhook_url,
+            headers: {
+                "Content-type": "application/json",
+            },
+            json: {
+                "channel": config.slack_channel,
+                "username": config.slack_username,
+                "icon_emoji": config.slack_icon,
+                "attachments": [
+                    {
+                        "fallback": "Message",
+                        "color": config.slack_color_good,
+                        "fields": [
+                            {
+                                "title": config.slack_title_good,
+                                "value": formatted + "\n" + url + "",
+                                "short": false
+                            }
+                        ]
+                    }
+                ]
+            }
+        };
+        request.post(options, function (error, response, body) { });
+    } else {
+        var options = {
+            uri: config.slack_webhook_url,
+            headers: {
+                "Content-type": "application/json",
+            },
+            json: {
+                "channel": config.slack_channel,
+                "username": config.slack_username,
+                "icon_emoji": config.slack_icon,
+                "attachments": [
+                    {
+                        "fallback": "Message",
+                        "color": config.slack_color_bad,
+                        "fields": [
+                            {
+                                "title": config.slack_title_bad,
+                                "value": formatted + "\n" + url + "\n" + result.error,
+                                "short": false
+                            }
+                        ]
+                    }
+                ]
+            }
+        };
+        request.post(options, function (error, response, body) { });
+    }
+}
+
+// Return HTTP Status Code.(and error code if exists.)
+function getHTTPstatuscode(url) {
+    return new Promise(function (resolve) {
+        request(url, { timeout: 5000 }, (err, res) => {
+            if (err !== null) {
+                resolve({ "code": 0, "error": err.code })
+                return;
+            }
+            resolve({ "code": res.statusCode });
+        });
+    })
+}
